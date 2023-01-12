c8501d474ad7e7e3fb84c19a755301ae614c03b8
diantaufik1 <diataufik1999@gmail.com>
2023-01-12 11:08:35 +0700

    first commit

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -7,7 +7,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body class="p-5">
-    <div class="container">
+    <!-- Nav  -->
+    <nav class="navbar navbar-expand-lg navbg shadow-sm bg-body rounded">
+        <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" width="100" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ">
+            <li class="nav-item">
+                <a class="nav-link round" href="/buku">Buku</a>
+                <li class="nav-item">
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button type="submit" class="nav-link round">Logout</button>
+                    </form>
+                </li>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <!-- akhir Nav  -->
+
+    <div class="container mt-5">
         <div class="row mb-5">
             <div class="col">
                 <a href="/tambah" class="btn btn-primary">Tambah buku</a>
